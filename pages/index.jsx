8097f7f1365812aa1ced85c772da3e19b5e98d7a
John1316 <johnmaher179@gmail.com>
2024-01-19 21:48:40 +0200

    update ne ui

--- pages/index.jsx
+++ pages/index.jsx
@@ -42,37 +42,37 @@ export default function index() {
   }
   function formatDate(inputDate) {
     const options = {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
+      year: "numeric",
+      month: "long",
+      day: "numeric",
     };
-    
+
     const date = new Date(inputDate);
-    const formattedDate = date.toLocaleDateString('en-US', options);
-    
+    const formattedDate = date.toLocaleDateString("en-US", options);
+
     const timeOptions = {
-      hour: 'numeric',
-      minute: 'numeric',
+      hour: "numeric",
+      minute: "numeric",
       hour12: true,
     };
-    
-    const formattedTime = date.toLocaleTimeString('en-US', timeOptions);
-    
+
+    const formattedTime = date.toLocaleTimeString("en-US", timeOptions);
+
     return `${formattedDate} - ${formattedTime}`;
   }
-    // Function to check if the "About Us" section is in the scroll view
-    function checkAboutUsInView() {
-      if (aboutUsRef.current) {
-        const rect = aboutUsRef.current.getBoundingClientRect();
-        const isInView = rect.top >= 0 && rect.bottom <= window.innerHeight;
-        setAboutUsActive(isInView);
-      }
+  // Function to check if the "About Us" section is in the scroll view
+  function checkAboutUsInView() {
+    if (aboutUsRef.current) {
+      const rect = aboutUsRef.current.getBoundingClientRect();
+      const isInView = rect.top >= 0 && rect.bottom <= window.innerHeight;
+      setAboutUsActive(isInView);
     }
-  
-    // Event listener to check the "About Us" section's visibility on scroll
-    function handleScroll() {
-      checkAboutUsInView();
-    }
+  }
+
+  // Event listener to check the "About Us" section's visibility on scroll
+  function handleScroll() {
+    checkAboutUsInView();
+  }
   useEffect(() => {
     if (!route.isReady) {
       return;
@@ -101,8 +101,6 @@ export default function index() {
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-    // getToken()
-    return () => {};
   }, [route, token]);
 
   return (
@@ -116,7 +114,6 @@ export default function index() {
       ) : token ? (
         <section className="meeting mh-100vh pt-[8px]">
           {meeting?.error ? (
-            
             <HomeEmpty />
           ) : (
             <div className={`card card__empty card__meeting`}>
@@ -154,13 +151,83 @@ export default function index() {
         </section>
       ) : (
         <>
-            <div className="signup_alert">
-              <p>If you're experiencing difficulties achieving your academic objectives and seeking an individual to comprehend your situation
-              thoroughly and devise a career strategy for you, EMPWR360 is the ideal platform to explore.</p>
-              <Button as={Link} className="navbar__signup" href="/create-account" variant="flat">
-                sign up
-              </Button>
+          <div className="signup_alert">
+            <p>
+              If you're experiencing difficulties achieving your academic
+              objectives and seeking an individual to comprehend your situation
+              thoroughly and devise a career strategy for you, EMPWR360 is the
+              ideal platform to explore.
+            </p>
+            <Button
+              as={Link}
+              className="navbar__signup"
+              href="/create-account"
+              variant="flat"
+            >
+              sign up
+            </Button>
+          </div>
+
+
+          <section className="platform mt-[24px]">
+            <div className="platform__content">
+              <h2 className="platform__content--title">
+                OUR PLATFORM
+              </h2>
+              <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 justify-center gap-[12px]">
+                <div className="platform__content--card">
+                  <h4 className="platform__content--card-name">
+                    Comprehensive Assessments
+                  </h4>
+                  <p className="platform__content--card-text">
+                    "Empower360 has been instrumental in shaping my career
+                    trajectory. Through their comprehensive assessments like The
+                    Learning Receptiveness Profiles test and The NAP Student
+                    Performer Brain Profile Assessment, I gained valuable
+                    insights into my unique learning style and cognitive
+                    strengths. Their guidance aligned my interests with my
+                    neural architecture, providing a clear direction for my
+                    future career. Thanks to Empower360, I now approach my
+                    professional journey with confidence and purpose."
+                  </p>
+                </div>
+                <div className="platform__content--card">
+                  <SiteImage 
+                  src={'/assets/images/folder-new-design/2.svg'}
+                  />
+                </div>
+                <div className="platform__content--card">
+                  <h4 className="platform__content--card-name">
+                    Tailored Guidance
+                  </h4>
+                  <p className="platform__content--card-text">
+                    "Empower360 has been instrumental in shaping my career
+                    trajectory. Through their comprehensive assessments like The
+                    Learning Receptiveness Profiles test and The NAP Student
+                    Performer Brain Profile Assessment, I gained valuable
+                    insights into my unique learning style and cognitive
+                    strengths. Their guidance aligned my interests with my
+                    neural architecture, providing a clear direction for my
+                    future career. Thanks to Empower360, I now approach my
+                    professional journey with confidence and purpose."
+                  </p>
+                </div>
+              </div>
+              <div className="flex  justify-center gap-[12px]">
+
+                <div className="w-1/2  platform__content--card mt-[0px]">
+                  <h4 className="platform__content--card-name mt-[0px]">
+                    Personalized Career Recommendations
+                  </h4>
+                  <p className="platform__content--card-text">
+                    The platform offers personalized career recommendations and strategies for success
+                    based on the results of these assessments, helping students make informed decisions
+                    about their future careers.
+                  </p>
+                </div>
+              </div>
             </div>
+          </section>
           {/* <section className="homecover">
             <div className="lg:flex justify-center items-center">
               <div className="homecover__content lg:w-8/12 md:w-12/12 md:mb-[40px]">
@@ -224,66 +291,80 @@ export default function index() {
               </div>
             </div>
           </section> */}
-          <section className="partners">
+          <section className="partners mt-[24px]">
             <div className="partners__content">
               <h2 className="partners__content--title">OUR PARTNERS</h2>
               <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-[12px]">
                 <div className="partners__content--image">
-                  <SiteImage src={'/assets/images/folder-new-design/7.svg'} />
+                  <SiteImage src={"/assets/images/folder-new-design/3.svg"} />
                 </div>
                 <div className="partners__content--image">
-                  <SiteImage src={'/assets/images/folder-new-design/6.svg'} />
+                  <SiteImage src={"/assets/images/folder-new-design/4.svg"} />
                 </div>
                 <div className="partners__content--image">
-                  <SiteImage src={'/assets/images/folder-new-design/5.svg'} />
+                  <SiteImage src={"/assets/images/folder-new-design/5.svg"} />
                 </div>
               </div>
             </div>
           </section>
-          <section className="satisfied">
+          <section className="satisfied mt-[24px]">
             <div className="satisfied__content">
-              <h2 className="satisfied__content--title">OUR SATISFIED CUSTOMERS</h2>
+              <h2 className="satisfied__content--title">
+                OUR SATISFIED CUSTOMERS
+              </h2>
               <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-[12px]">
                 <div className="satisfied__content--card">
-                  <p className="satisfied__content--card-text">"Empower360 has been instrumental in shaping my career
+                  <p className="satisfied__content--card-text">
+                    "Empower360 has been instrumental in shaping my career
                     trajectory. Through their comprehensive assessments like The
                     Learning Receptiveness Profiles test and The NAP Student
-                    Performer Brain Profile Assessment, I gained valuable insights
-                    into my unique learning style and cognitive strengths. Their
-                    guidance aligned my interests with my neural architecture,
-                    providing a clear direction for my future career. Thanks to
-                    Empower360, I now approach my professional journey with
-                    confidence and purpose."</p>
-                  <h4 className="satisfied__content--card-name">-Noor Al-Jannahi</h4>
+                    Performer Brain Profile Assessment, I gained valuable
+                    insights into my unique learning style and cognitive
+                    strengths. Their guidance aligned my interests with my
+                    neural architecture, providing a clear direction for my
+                    future career. Thanks to Empower360, I now approach my
+                    professional journey with confidence and purpose."
+                  </p>
+                  <h4 className="satisfied__content--card-name">
+                    -Noor Al-Jannahi
+                  </h4>
                 </div>
                 <div className="satisfied__content--card">
-                  <p className="satisfied__content--card-text">"Empower360 has been instrumental in shaping my career
+                  <p className="satisfied__content--card-text">
+                    "Empower360 has been instrumental in shaping my career
                     trajectory. Through their comprehensive assessments like The
                     Learning Receptiveness Profiles test and The NAP Student
-                    Performer Brain Profile Assessment, I gained valuable insights
-                    into my unique learning style and cognitive strengths. Their
-                    guidance aligned my interests with my neural architecture,
-                    providing a clear direction for my future career. Thanks to
-                    Empower360, I now approach my professional journey with
-                    confidence and purpose."</p>
-                  <h4 className="satisfied__content--card-name">-Noor Al-Jannahi</h4>
+                    Performer Brain Profile Assessment, I gained valuable
+                    insights into my unique learning style and cognitive
+                    strengths. Their guidance aligned my interests with my
+                    neural architecture, providing a clear direction for my
+                    future career. Thanks to Empower360, I now approach my
+                    professional journey with confidence and purpose."
+                  </p>
+                  <h4 className="satisfied__content--card-name">
+                    -Noor Al-Jannahi
+                  </h4>
                 </div>
                 <div className="satisfied__content--card">
-                  <p className="satisfied__content--card-text">"Empower360 has been instrumental in shaping my career
+                  <p className="satisfied__content--card-text">
+                    "Empower360 has been instrumental in shaping my career
                     trajectory. Through their comprehensive assessments like The
                     Learning Receptiveness Profiles test and The NAP Student
-                    Performer Brain Profile Assessment, I gained valuable insights
-                    into my unique learning style and cognitive strengths. Their
-                    guidance aligned my interests with my neural architecture,
-                    providing a clear direction for my future career. Thanks to
-                    Empower360, I now approach my professional journey with
-                    confidence and purpose."</p>
-                  <h4 className="satisfied__content--card-name">-Noor Al-Jannahi</h4>
+                    Performer Brain Profile Assessment, I gained valuable
+                    insights into my unique learning style and cognitive
+                    strengths. Their guidance aligned my interests with my
+                    neural architecture, providing a clear direction for my
+                    future career. Thanks to Empower360, I now approach my
+                    professional journey with confidence and purpose."
+                  </p>
+                  <h4 className="satisfied__content--card-name">
+                    -Noor Al-Jannahi
+                  </h4>
                 </div>
               </div>
             </div>
           </section>
-          <section id="about" ref={aboutUsRef}  className="what">
+          <section id="about" ref={aboutUsRef} className="what mt-[24px]">
             {/* <div className="grid lg:grid-cols-2 md:grid-cols-1 justify-center gap-[120px] items-center">
               <SiteImage src="/assets/images/Picture2.svg" className="w-full" />
               <div className="what__content ">
@@ -299,57 +380,73 @@ export default function index() {
             </div> */}
             <div className="grid grid-cols-6">
               <div className="col-span-1">
-                <SiteImage className="what__image" src={'/assets/images/folder-new-design/10.svg'} />
+                <SiteImage
+                  className="what__image"
+                  src={"/assets/images/folder-new-design/6.svg"}
+                />
               </div>
               <div className="col-span-5">
                 <div className="">
                   <h4 className="what__content--header">OUR STORY</h4>
-                  <p className="what__content--text">Empower360 was created by a group of UAE graduates who experienced the challenges students faced in making career decisions. The platform combines science-based assessments with personalized guidance from internationally known companies specialized in professional development. Empower360 aims to help students confidently navigate their career paths and find fulfillment in the ever-changing job market.
+                  <p className="what__content--text">
+                    Empower360 was created by a group of UAE graduates who
+                    experienced the challenges students faced in making career
+                    decisions. The platform combines science-based assessments
+                    with personalized guidance from internationally known
+                    companies specialized in professional development.
+                    Empower360 aims to help students confidently navigate their
+                    career paths and find fulfillment in the ever-changing job
+                    market.
                   </p>
                 </div>
-
               </div>
             </div>
           </section>
-          <section  className="vision">
+          <section className="vision">
             <div className="grid grid-cols-2">
               <div className="grid grid-cols-6 gap-[8px]">
                 <div className="col-span-1">
-                  <SiteImage className="vision__image" src={'/assets/images/folder-new-design/9.svg'} />
+                  <SiteImage
+                    className="vision__image"
+                    src={"/assets/images/folder-new-design/7.svg"}
+                  />
                 </div>
                 <div className="col-span-5">
                   <div className="">
                     <h4 className="vision__content--header">OUR VISION</h4>
-                      <p className="vision__content--text">Empower360 aims to be the top science-driven career
-  guidance platform, helping students worldwide pursue fulfilling
-  careers that match their potential and transforming career
-  decision-making.
+                    <p className="vision__content--text">
+                      Empower360 aims to be the top science-driven career
+                      guidance platform, helping students worldwide pursue
+                      fulfilling careers that match their potential and
+                      transforming career decision-making.
                     </p>
                   </div>
-
                 </div>
               </div>
-            <div className="grid grid-cols-6 gap-[12px]">
-              <div className="col-span-1">
-                <SiteImage className="vision__image" src={'/assets/images/folder-new-design/8.svg'} />
-              </div>
-              <div className="col-span-5">
-                <div className="">
-                  <h4 className="vision__content--header"> OUR MISSION</h4>
-                  <p className="vision__content--text">Empower360 aims to revolutionize student career success by
-providing science-based assessments and personalized guidance
-to help students discover their unique strengths and interests.
-Their goal is to boost confidence, improve academic outcomes,
-and equip students with the knowledge to make informed career
-decisions in today's dynamic job market
-                  </p>
+              <div className="grid grid-cols-6 gap-[12px]">
+                <div className="col-span-1">
+                  <SiteImage
+                    className="vision__image"
+                    src={"/assets/images/folder-new-design/8.svg"}
+                  />
                 </div>
-
-              </div>
+                <div className="col-span-5">
+                  <div className="">
+                    <h4 className="vision__content--header"> OUR MISSION</h4>
+                    <p className="vision__content--text">
+                      Empower360 aims to revolutionize student career success by
+                      providing science-based assessments and personalized
+                      guidance to help students discover their unique strengths
+                      and interests. Their goal is to boost confidence, improve
+                      academic outcomes, and equip students with the knowledge
+                      to make informed career decisions in today's dynamic job
+                      market
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
           </section>
-
         </>
       )}
     </MainLayout>
